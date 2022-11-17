463499089ebacb929e1dd89c35fe228ad421414d
stevecyj <ycsteven.tsao@gmail.com>
2022-11-17 17:55:17 +0800

    lookbehind

---
+++ 2-2.js
@@ -0,0 +1,9 @@
+// let result = 'hello'.replace(/^|$/g, '#');
+
+// console.log(result);
+
+// let res = '[JS] Lesson_01.mp4'.replace(/\b/g, '#');
+// console.log(res);
+
+let res = 'hello'.replace(/(?!l)/g, '#');
+console.log(res);
