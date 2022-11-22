c39f164b52c821feeaee481e78f78682b433e0db
stevecyj <ycsteven.tsao@gmail.com>
2022-11-22 09:28:23 +0800

    fuzzy match

--- 1-1fuzzyMatch.js
+++ 1-1fuzzyMatch.js
@@ -1,3 +1,6 @@
+let regexA = /ab{2,5}c/g;
 let regex = /a[123]b/g;
+let string_a = 'abc abbc abbbc abbbbc abbbbbc abbbbbbc';
 let string = 'a0b a1b a2b a3b a4b';
-console.log(string.match(regex));
+// console.log(string.match(regex));
+console.log(string_a.match(regexA));
