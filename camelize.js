893030c17507465c0feaa4902afb12e18eea2118
stevecyj <ycsteven.tsao@gmail.com>
2022-11-28 12:57:36 +0800

    doc:camelize comments

--- camelize.js
+++ camelize.js
@@ -1,7 +1,10 @@
+// (.)單詞的第一個字母
+// ?，單詞結尾可能有空白
+
 function camelize(str) {
   return str.replace(/[-_\s]+(.)?/g, function (match, c) {
     return c ? c.toUpperCase() : '';
   });
 }
 
-console.log(camelize('moz-transform '));
+console.log(camelize('-moz_transform '));
