8e4d9f3130782c5af7097ba39a82d6b6762f2a68
stevecyj <ycsteven.tsao@gmail.com>
2022-11-29 15:12:03 +0800

    feat:取得輸入的文字、正規表達式

--- realcase/tester.js
+++ realcase/tester.js
@@ -5,5 +5,37 @@
   let errBox = document.getElementById('err');
   let resultBox = document.getElementById('result');
 
-  
+  // button event
+  runBtn.onclick = function () {
+    // 歸零
+    errBox.innerHTML = '';
+    resultBox.innerHTML = '';
+
+    // 取得 input 內容
+    let text = textInput.value;
+    let regex = regexInput.value;
+  };
+
+  // input 提示
+  if (regex == '') {
+    errBox.innerHTML = '請輸入正則表達式';
+  } else if (text == '') {
+    errBox.innerHTML = '請輸入測試內容';
+  } else {
+    regex = createRegex(regex);
+    if (!regex) {
+      return;
+    }
+    let result,
+      results = [];
+  }
+
+  // 沒有修飾符 g, 會無限循環
+  if (regex.global) {
+    while (result=regex.exec(text)) {
+      results.push(result)
+    }
+  } else {
+    results.push(regex.exec(text))
+  } 
 });
