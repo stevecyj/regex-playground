52895aed5b8ee6e3255cf821812469f282adb956
stevecyj <ycsteven.tsao@gmail.com>
2022-11-29 14:50:44 +0800

    realcase:reg tester init

---
+++ realcase/tester.js
@@ -0,0 +1,9 @@
+(function () {
+  let regexInput = document.getElementById('regex');
+  let textInput = document.getElementById('text');
+  let runBtn = document.getElementById('run');
+  let errBox = document.getElementById('err');
+  let resultBox = document.getElementById('result');
+
+  
+});
